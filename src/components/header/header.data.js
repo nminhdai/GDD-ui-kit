9a03c1af05b0f2bc3ca33f151f7dbcb5fd7e5f62
nminhdai <ngominhdai92tn@gmail.com>
2023-04-16 10:40:29 +0700

    init

---
+++ src/components/header/header.data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    path: "banner",
+    label: "Home",
+  },
+  {
+    path: "services",
+    label: "Services",
+  },
+  {
+    path: "pricing",
+    label: "Pricing",
+  },
+  {
+    path: "testimonials",
+    label: "Testimonials",
+  },
+  {
+    path: "news",
+    label: "News",
+  },
+  {
+    path: "contact",
+    label: "Contact",
+  },
+  {
+    path: "about",
+    label: "About us",
+  },
+];
